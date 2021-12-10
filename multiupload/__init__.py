73bafcf3c0aa1fe320e92ab9a98dfe98b9e0b5d9
gikki123 <67251774+gikki123@users.noreply.github.com>
2021-12-10 17:56:56 +0530

    Update __init__.py

--- multiupload/__init__.py
+++ multiupload/__init__.py
@@ -11,4 +11,4 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.INFO)
 
 
-anjana = TelegramClient('anjana', api_id=Config.API_ID, api_hash=Config.API_HASH).start(bot_token=Config.BOT_TOKEN) 
+anjana = TelegramClient('anjana', api_id = Config.API_ID, api_hash = Config.API_HASH).start(bot_token=Config.BOT_TOKEN) 
